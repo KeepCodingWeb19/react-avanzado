3a323c67b383ad7669ac837bfa2331f997dca2cf
Àlex Martínez <hey@alexmdevs.com>
2026-02-16 20:03:39 +0100

    init nextjs project template

---
+++ src/app/app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  console.log("Hello world");
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+  );
+}
